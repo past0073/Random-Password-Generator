c6c24ccba16f312ed59e01031abc565207815a0a
AP <past0073@umn.edu>
2020-12-23 19:36:24 -0600

    Added prompt to gather user's desired password length, and created a check to ensure the criteria is met.

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var pwLength = prompt("How many characters should your password be? Select a number between 8 and 128.");
+  while (pwLength < 8 || pwLength > 128 || pwLength === "") {
+    var pwLength = prompt("You must choose a number between 8 and 128.")
+    if (pwLength > 8 && pwLength < 128) break;
+    }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
