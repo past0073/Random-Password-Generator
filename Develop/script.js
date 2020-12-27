b4e6331e2ebc717056d4fa23560124fe17cb2fa3
AP <past0073@umn.edu>
2020-12-26 21:26:59 -0600

    Added arrays for all character types, and began the process of generating the password.

--- Develop/script.js
+++ Develop/script.js
@@ -8,10 +8,12 @@ function writePassword() {
     var pwLength = prompt("You must choose a number between 8 and 128.")
     if (pwLength > 8 && pwLength < 128) break;
     }
+
   var upperCase = confirm("Do you want your password to include uppercase characters?");
   var lowerCase = confirm("Do you want your password to include lowercase characters?");
   var numbers = confirm("Do you want your password to include numbers?");
   var special = confirm("Do you want your password to include speical characters?");
+
   while (upperCase === false && lowerCase === false && numbers === false && special === false) {
     alert("You must select at least one character type.");
     var upperCase = confirm("Do you want your password to include uppercase characters?");
@@ -20,7 +22,34 @@ function writePassword() {
     var special = confirm("Do you want your password to include speical characters?");
     if (upperCase === true || lowerCase === true || numbers === true || special === true) break;
   }
+
   alert("Your password will be " + pwLength + " characters and will contain the following types of characters: Uppercase: " + upperCase + ", Lowercase: " + lowerCase + ", Numbers: " + numbers + ", and Special Characters: " + special + ".")
+  
+  var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialArr =  ["!", "#", "$", "%","&","'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@"];
+  var pwLength = parseInt(pwLength)
+ 
+function generatePassword () {
+ for (i=0; i<pwLength; i++) {
+
+    ranUpper = upperArr[Math.floor(Math.random() * upperArr.length)];
+    ranLower = lowerArr[Math.floor(Math.random() * lowerArr.length)];
+    ranNum = numArr[Math.floor(Math.random() * numArr.length)];
+    ranSpecial = specialArr[Math.floor(Math.random() * specialArr.length)];
+ 
+
+    passwordArray = [ranUpper, ranLower, ranNum, ranSpecial];
+    passwordText = passwordArray[Math.floor(Math.random() * pwLength)];
+    console.log(passwordText);
+    
+ }
+ }
+
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
